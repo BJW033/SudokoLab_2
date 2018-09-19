380763fd1db0dbbc342ff7337aead6f677df7947
Brandon Wu <Brandon Wu@128.4.105.179>
2018-09-18 20:34:12 -0400

    commit

--- SudokuBLL/src/main/java/pkgHelper/Sudoku.java
+++ SudokuBLL/src/main/java/pkgHelper/Sudoku.java
@@ -1,5 +1,23 @@
 package pkgHelper;
-
+import java.math.*;
 public class Sudoku extends LatinSquare{
-	private int l;
+	private int iSize;
+	private int iSqrtSize;
+	
+	public Sudoku(int iSize){
+		// Make an exception if size is not a perfect square
+		super();
+		this.iSize = iSize;
+		this.iSqrtSize = (int) Math.sqrt(iSize);
+	}
+	public Sudoku(int[][] puzzle) {
+		// Make an exception if size is not a perfect square
+		super(puzzle);
+		this.iSize = puzzle.length;
+		this.iSqrtSize = (int) Math.sqrt(iSize);
+	}
+	protected int[][] getPuzzle() {
+		return super.getLatinSquare();
+	}
+	
 }
